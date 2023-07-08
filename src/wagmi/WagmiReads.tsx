71ae1ebf57e25ed43b5adc8e420d859b1dd5f901
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-07-08 11:19:40 -0700

    feat: Add EVMts Next
    feat(build): Migrate to pnpm
    
    chore: pnpm i @evmts/webpack-plugin @evmts/ts-plugin @evmts/config solc -d
    
    chore: pnpm i @evmts/core
    
    feat: Update tsconfig with the plugin
    
    fix: Fix configs
    
    feat: Add next config
    
    feat: Add wagmi examples
    
    fix: Missing key props
    
    chore: Nuke evmts.config.ts
    
    docs: update readme
    
    make it work
    
    Add pics
    
    feat: Add EVMts
    
    feat(build): Migrate to pnpm
    
    chore: pnpm i @evmts/webpack-plugin @evmts/ts-plugin @evmts/config solc -d
    
    chore: pnpm i @evmts/core
    
    feat: Update tsconfig with the plugin
    
    fix: Fix configs
    
    feat: Add next config
    
    feat: Add wagmi examples
    
    fix: Missing key props
    
    chore: Nuke evmts.config.ts
    
    docs: update readme
    
    make it work
    
    Update README.md
    
    Fix images in readme
    
    fix: Fix bug with forgetting to pass in chainId
    
    Update README.md

---
+++ src/wagmi/WagmiReads.tsx
@@ -0,0 +1,48 @@
+import { WagmiMintExample } from '../contracts/WagmiMintExample.sol'
+import { Address, useAccount, useContractRead, useNetwork } from 'wagmi'
+
+export const WagmiReads = () => {
+	const { chain } = useNetwork()
+	// in future versian of evmts this will work without casting to string
+	const chainId = String(chain?.id ?? '1') as '1' | '420'
+
+	const { address, isConnected } = useAccount()
+
+	const { data: balance } = useContractRead({
+		/**
+		 * Spreading in a method will spread abi, address and args
+		 * Hover over balanceOf and click go-to-definition should take you to the method definition in solidity if compiling from solidity
+		 */
+		...WagmiMintExample.read({ chainId }).balanceOf(address as Address),
+		enabled: isConnected,
+	})
+	const { data: totalSupply } = useContractRead({
+		...WagmiMintExample.read({ chainId }).totalSupply(),
+		enabled: isConnected,
+	})
+	const { data: tokenUri } = useContractRead({
+		...WagmiMintExample.read({ chainId }).tokenURI(BigInt(1)),
+		enabled: isConnected,
+	})
+	const { data: symbol } = useContractRead({
+		...WagmiMintExample.read({ chainId }).symbol(),
+		enabled: isConnected,
+	})
+	const { data: ownerOf } = useContractRead({
+		...WagmiMintExample.read({ chainId }).ownerOf(BigInt(1)),
+		enabled: isConnected,
+	})
+	return (
+		<div>
+			<div>
+				<div>
+					balanceOf({address}): {balance?.toString()}
+				</div>
+				<div>totalSupply(): {totalSupply?.toString()}</div>
+				<div>tokenUri(BigInt(1)): {tokenUri?.toString()}</div>
+				<div>symbol(): {symbol?.toString()}</div>
+				<div>ownerOf(BigInt(1)): {ownerOf?.toString()}</div>
+			</div>
+		</div>
+	)
+}
---
+++ src/wagmi/WagmiWrites.tsx
@@ -0,0 +1,59 @@
+import { WagmiMintExample } from '../contracts/WagmiMintExample.sol'
+import { getRandomInt } from '../utils/getRandomInt'
+import {
+	Address,
+	useAccount,
+	useContractRead,
+	useContractWrite,
+	useNetwork,
+	useWaitForTransaction,
+} from 'wagmi'
+
+export const WagmiWrites = () => {
+	const { chain } = useNetwork()
+	// in future versian of evmts this will work without casting to string
+	const chainId = String(chain?.id ?? '1') as '1' | '420'
+
+	const { address, isConnected } = useAccount()
+
+	const { data, refetch } = useContractRead({
+		/**
+		 * Spreading in a method will spread abi, address and args
+		 * Hover over balanceOf and click go-to-definition should take you to the method definition in solidity if compiling from solidity
+		 */
+		...WagmiMintExample.read({ chainId }).balanceOf(address as Address),
+		enabled: isConnected,
+	})
+
+	const { writeAsync: writeMint, data: mintData } = useContractWrite({
+		/**
+		 * Not calling the function will return abi and address without args
+		 * This is useful for when you want to lazily call the function like in case of useContractWrite
+		 */
+		...WagmiMintExample.write({ chainId }).mint,
+	})
+
+	useWaitForTransaction({
+		hash: mintData?.hash,
+		onSuccess: (receipt) => {
+			console.log('minted', receipt)
+			refetch()
+		},
+	})
+
+	return (
+		<div>
+			<div>
+				<div>balance: {data?.toString()}</div>
+			</div>
+			<button
+				type='button'
+				onClick={() =>
+					writeMint(WagmiMintExample.write({ chainId }).mint(BigInt(getRandomInt())))
+				}
+			>
+				Mint
+			</button>
+		</div>
+	)
+}
