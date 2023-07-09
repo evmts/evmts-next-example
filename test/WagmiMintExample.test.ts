53eb0a01ad7edbeca67be10888ff3c820a73dd75
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-07-08 21:02:54 -0700

    feat: Pnpm up evmts

--- test/WagmiMintExample.test.ts
+++ test/WagmiMintExample.test.ts
@@ -4,8 +4,6 @@ import { run } from "hardhat";
 import { createPublicClient, createWalletClient, http, Address } from "viem";
 import { hardhat } from "viem/chains";
 
-import { abi, bytecode } from "../out/WagmiMintExample.sol/WagmiMintExample.json";
-
 export const publicClient = createPublicClient({
   chain: hardhat,
   transport: http(),
@@ -21,16 +19,14 @@ beforeAll(() => {
 
 });
 
-describe("WagmiMintExample", function () {
-  it("Should return run correct function parameters on hardhat", async function () {
+describe("WagmiMintExample", function() {
+  it("Should return run correct function parameters on hardhat", async function() {
     console.log(await publicClient.getBlockNumber());
-    const accounts = await walletClient.getAddresses();
-    const [account, ...other] = accounts;
+    const [account] = await walletClient.getAddresses();
     const hash = await walletClient.deployContract({
-      abi: abi,
-      bytecode: bytecode.object as `0x${string}`,
-      account: accounts[0],
-      args: [],
+      abi: WagmiMintExample.abi,
+      bytecode: WagmiMintExample.bytecode as `0x${string}`,
+      account,
     });
     const receipt = await publicClient.waitForTransactionReceipt({ hash });
     console.log(receipt.contractAddress);
