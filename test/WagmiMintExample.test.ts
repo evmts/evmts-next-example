c60406918ff5a9e2dccf600a0ce65c725ecd9647
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-07-09 23:44:21 -0700

    fix: Make test only deploy

--- test/WagmiMintExample.test.ts
+++ test/WagmiMintExample.test.ts
@@ -1,5 +1,5 @@
-import { beforeAll, describe, it, expect } from "vitest";
-import { WagmiMintExample } from '../src/contracts/WagmiMintExample.sol'
+import { beforeAll, describe, it } from "vitest";
+import { WagmiMintExample } from "../src/contracts/WagmiMintExample.sol";
 import { run } from "hardhat";
 import { createPublicClient, createWalletClient, http, Address } from "viem";
 import { hardhat } from "viem/chains";
@@ -9,28 +9,21 @@ export const publicClient = createPublicClient({
   transport: http(),
 });
 
-export const walletClient = createWalletClient({
-  chain: hardhat,
-  transport: http(),
-});
-
 beforeAll(() => {
   run("node");
-
 });
 
-describe("WagmiMintExample", function() {
-  it("Should return run correct function parameters on hardhat", async function() {
-    console.log(await publicClient.getBlockNumber());
+describe("WagmiMintExample", function () {
+  it("Should deploy", async function () {
+    const walletClient = createWalletClient({
+      chain: hardhat,
+      transport: http(),
+    });
     const [account] = await walletClient.getAddresses();
     const hash = await walletClient.deployContract({
       abi: WagmiMintExample.abi,
       bytecode: WagmiMintExample.bytecode as `0x${string}`,
       account,
     });
-    const receipt = await publicClient.waitForTransactionReceipt({ hash });
-    console.log(receipt.contractAddress);
-    console.log(WagmiMintExample.read({ chainId: `${hardhat.id}` }).balanceOf(
-      `0x0000000000FFe8B47B3e2130213B802212439497` as Address))
   });
 });
